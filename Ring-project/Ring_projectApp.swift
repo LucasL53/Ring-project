e61a079cfbe966206697cb06732a47af09325fe0
eyoel gebre <eyoelge12@gmail.com>
2024-02-15 00:37:07 -0800

    Initial Commit

---
+++ Ring-project/Ring_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ring_projectApp.swift
+//  Ring-project
+//
+//  Created by Eyoel Gebre on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Ring_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
